8dbaa740ad50389f8dd6e5e7a9f6de9819db015f
Alec Lichtenberger <lichtenbergeralec@gmail.com>
2026-08-25 11:04:00 -0700

    Adjust my get_accel code to match a similar process as the get quaternion code that existed the whole time and i could of used as a template instead of crawling through the datasheet for 3 hours

--- mini-repo/core/util/peripherals/imu/BNO055.cpp
+++ mini-repo/core/util/peripherals/imu/BNO055.cpp
@@ -85,27 +85,42 @@ void BNO055::reset() noexcept{
  * @param la pointer to the x,y,z of the imu
  */
 void BNO055::get_accel(BNO055_VECTOR_TypeDef *la){
+    if (cantReadDataCount > 0 && cantReadDataCount < 50) {
+        cantReadDataCount++;
+        return;
+    } else if (cantReadDataCount >= 50) {
+        cantReadDataCount = 1;
+    }
     char writeArr [1] = {0x08};
-    _i2c.write(chip_addr, writeArr, 1, false);
-    _i2c.read(0x51, dt, 6, true); // Reads the contents of the accel registers into dt
+    int writeResult = _i2c.write(chip_addr, writeArr, 1, false);
+    
+    if (!writeResult)  {
+        if (cantReadDataCount > 0) {
+            printf("RESET IMU\n");
+            reset();
+            cantReadDataCount = 0;
+        }
+        _i2c.read(chip_addr, dt, 6, true); // Reads the contents of the accel registers into dt
+        
+        //Move contents of dt into the Vector Type Def struct via the given pointer to the struct
+        int x_LSB = dt[0];
+        int x_MSB = dt[1];
+        int y_LSB = dt[2];
+        int y_MSB = dt[3];
+        int z_LSB = dt[4];
+        int z_MSB = dt[5];
+        
+        //bit shifting
+        uint16_t accel_X = (x_MSB << 8) | x_LSB;
+        uint16_t accel_Y = (y_MSB << 8) | y_LSB;
+        uint16_t accel_Z = (z_MSB << 8) | z_LSB;
 
-    //Move contents of dt into the Vector Type Def struct via the given pointer to the struct
-    int x_LSB = dt[0];
-    int x_MSB = dt[1];
-    int y_LSB = dt[2];
-    int y_MSB = dt[3];
-    int z_LSB = dt[4];
-    int z_MSB = dt[5];
-
-    //bit shifting
-    uint16_t accel_X = (x_MSB << 8) | x_LSB;
-    uint16_t accel_Y = (y_MSB << 8) | y_LSB;
-    uint16_t accel_Z = (z_MSB << 8) | z_LSB;
-
-    //Load into the passed struct
-    la-> x = accel_X;
-    la-> y = accel_Y;
-    la-> z = accel_Z;
+        //Load into the passed struct
+        la-> x = accel_X;
+        la-> y = accel_Y;
+        la-> z = accel_Z;
+        cantReadDataCount++;
+    }
 }
 
 /**
@@ -115,8 +130,8 @@ void BNO055::get_accel(BNO055_VECTOR_TypeDef *la){
  */
 void BNO055::get_gyro(BNO055_VECTOR_TypeDef *gr){
     char writeArr [1] = {0x0E};
-    _i2c.write(chip_addr, writeArr, 1, false);
-    _i2c.read(0x51, dt, 6, true); // Reads the contents of the accel registers into dt
+    int writeResult = _i2c.write(chip_addr, writeArr, 1, false);
+    _i2c.read(chip_addr, dt, 6, true); // Reads the contents of the accel registers into dt
 
     //Move contents of dt into the Vector Type Def struct via the given pointer to the struct
     int x_LSB = dt[0];
