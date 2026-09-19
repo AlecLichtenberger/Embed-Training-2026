6027ed8b4352791df73dee92196d77ff488d6217
Alec Lichtenberger <lichtenbergeralec@gmail.com>
2026-09-19 00:10:14 -0700

    Mode C implemented

--- mini-repo/robots/Infantry/Infantry.cpp
+++ mini-repo/robots/Infantry/Infantry.cpp
@@ -213,6 +213,11 @@ class Infantry : public BaseRobot {
             des_turret_state.turret_mode = TurretState::AIM;
             
         }
+        else if (mode == DJIRemote2::ModeSwitch::MODE_C){
+            des_chassis_state.vOmega = 0;
+            chassis_.setChassisSpeeds(des_chassis_state,ChassisSubsystem::DRIVE_MODE::YAW_ORIENTED);
+            des_turret_state.turret_mode = TurretState::AIM;
+        }   
         else{
 
             chassis_.setWheelPower({0,0,0,0});
